8e42d2a61be7a941dc74202003c74619e3ab9de6
MashaShamash <mariapanasenko26@yandex.ru>
2024-07-12 12:21:06 +0300

    done

---
+++ client/src/app/provider/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+type AppRoutesProps = {};
+function AppRoutes(): JSX.Element {
+  return (
+    <Routes>
+      <Route path='/'/> 
+      <Route path='/signIn'/> 
+      <Route path='/signUp'/> 
+      <Route path='/profile'/> 
+    </Routes>
+  );
+}
+export default AppRoutes;
---
+++ client/src/app/store/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+   
+
+  },
+});
+
+
+export type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+export type StoreType = typeof store;
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+export default store;
