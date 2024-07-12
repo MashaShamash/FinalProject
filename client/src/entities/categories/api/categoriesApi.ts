73100a276fb30d2b41a835c1ae68517cd3332b95
IEKMA <cherentsovak@gmail.com>
2024-07-12 14:55:33 +0300

    entitiesCategories

---
+++ client/src/entities/categories/api/categoriesApi.ts
@@ -0,0 +1,7 @@
+import { AxiosResponse } from "axios"
+import axiosInstance from "../../../services/axiosInstance"
+import { Category, CategoryId, CategoryWithoutIdAndUserId } from "../types/categoryTypesTypes";
+
+class CategoryApi{}
+
+export default CategoryApi
---
+++ client/src/entities/categories/ui/categoriesSlice.ts
@@ -0,0 +1,11 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import CategoryApi from "./api/categoryApi"
+import { Movie, CategoryId, CategoryWithoutIdAndUserId } from "./types/categoryTypes"
+
+
+
+
+const categorySlice =({})
+
+
+export default categorySlice
