377c35a44a11fc5603df7ad3681063f32f691810
IEKMA <cherentsovak@gmail.com>
2024-07-18 20:09:20 +0300

    done

--- client/src/page/MagazinPage/MagazinPage.tsx
+++ client/src/page/MagazinPage/MagazinPage.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Slider from 'rc-slider';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { useAppDispatch, useAppSelector } from '../../app/store/store';
+import { useAppSelector } from '../../app/store/store';
 import 'rc-slider/assets/index.css';
 import FigureItem from '../../entities/figures/ui/FigureItem';
 import type { Figure } from '../../entities/figures/types/figureTypes';
 import CategoryPage from '../CategoryPage/CategoryPage';
 import { Link } from 'react-router-dom';
 import './MagazinPage.css';
-import { loadBaskets } from '../../entities/basket/basketSlice';
 
 type PriceRange = [number, number];
 
@@ -17,10 +16,8 @@ type Figures = {
 };
 
 function MagazinPage(): JSX.Element {
-  const dispatch = useAppDispatch();
   const { categories } = useAppSelector((state) => state.categories);
   const { figures } = useAppSelector((state) => state.figures);
-  const user = useAppSelector((state) => state.auth.user);
   const [selectCategor, setSelectCategor] = useState<string>('');
   const [statePseudonym, setStatePseudonym] = useState<string>('');
   const [stateMaterial, setStateMaterial] = useState<string>('');
@@ -51,7 +48,7 @@ function MagazinPage(): JSX.Element {
     setStateHeight(value as PriceRange);
   };
 
-  const onHandleGetBasket = (): void => {
+  const onHandleGetBasket = () => {
     const filteredFigures = figures.filter((figure) => {
       const { pseudonym, materials, price, width, height, title, date, categoryId } = figure;
       return (
@@ -76,7 +73,7 @@ function MagazinPage(): JSX.Element {
 
     setItems(filteredFigures.slice(0, 20));
   };
-  const fetchMoreData = (): void => {
+  const fetchMoreData = () => {
     if (items.length >= figures.length) {
       setHasMore(false);
       return;
@@ -86,33 +83,27 @@ function MagazinPage(): JSX.Element {
     }, 1500);
   };
 
-  const onHeandleWrite = (): void => {
-    setSelectCategor('');
-    setStatePseudonym('');
-    setStateMaterial('');
-    setStateWidth([1, 7000]);
-    setStateHeight([1, 7000]);
-    setStateTitle('');
-    setStateDate([1, 2025]);
-    setPriceRange([100, 1000000]);
-    setFilteredFigures(figures);
-    setIsOpen(false);
-  };
-
-
+  const onHeandleWrite = () => {
+  setSelectCategor('')
+  setStatePseudonym('')
+  setStateMaterial('')
+  setStateWidth([1, 7000])
+  setStateHeight([1, 7000])
+  setStateTitle('')
+  setStateDate([1, 2025])
+  setPriceRange([100, 1000000])
+  setFilteredFigures(figures)
+  setIsOpen(false)
+  }
 
   return (
     <div className="wrappers">
       <div className="MagazinPage">
-
         <div className='sautBar'>
           <button style={{
   backgroundColor: 'white',
+  boxShadow: '4px 4px 8px rgb(196, 216, 194)'
 }} onClick={() => setIsExpanded(!isExpanded)}>
-
-        <div className="sautBar">
-       
-
             {isExpanded ? 'Скрыть' : 'Показать'} фильтры
           </button>
           <div className={`contener ${isExpanded ? 'active' : ''}`}>
@@ -163,10 +154,8 @@ function MagazinPage(): JSX.Element {
             <div className="conteiner-slider">
               <div  className="price">
                 <div style={{ marginTop: '20px' }}>
-
                   <p className="material-spa">Ширина картины: от {stateWidth[0]} до {stateWidth[1]} мм</p>
                   <Slider 
-
                     range
                     className="price-slider"
                     min={1}
@@ -179,9 +168,7 @@ function MagazinPage(): JSX.Element {
               </div>
               <div className="height">
                 <div style={{ marginTop: '20px' }}>
-                  <p className="material-spa">
-                    Высота картины: от {stateHeight[0]} до {stateHeight[1]} мм
-                  </p>
+                  <p className="material-spa">Высота картины: от {stateHeight[0]} до {stateHeight[1]} мм</p>
                   <Slider
                     range
                     className="height-slider"
@@ -195,9 +182,7 @@ function MagazinPage(): JSX.Element {
               </div>
               <div className="date">
                 <div style={{ marginTop: '20px' }}>
-                  <p className="material-spa">
-                    Дата: от {stateDate[0]} до {stateDate[1]} года
-                  </p>
+                  <p className="material-spa">Дата: от {stateDate[0]} до {stateDate[1]} года</p>
                   <Slider
                     range
                     min={0}
@@ -211,9 +196,7 @@ function MagazinPage(): JSX.Element {
               </div>
               <div className="price">
                 <div style={{ marginTop: '20px' }}>
-                  <p className="material-spa">
-                    Цена: от {priceRange[0]} до {priceRange[1]} рублей
-                  </p>
+                  <p className="material-spa">Цена: от {priceRange[0]} до {priceRange[1]} рублей</p>
                   <Slider
                     range
                     className="price-slider"
@@ -228,42 +211,42 @@ function MagazinPage(): JSX.Element {
             </div>
             <div className="buttonSearch">
               <button onClick={() => onHandleGetBasket()}>Найти</button>
-              <button onClick={() => onHeandleWrite()}>Очистить</button>
+              <button onClick={()=> onHeandleWrite()}>Очистить</button>
             </div>
           </div>
         </div>
-        <div className="hpo">
-          <div className="jhg">
-            <CategoryPage />
-          </div>
-          <div className="results">
-            {isOpen ? (
-              <h2>По вашему запросу найдено: {filteredFigures.length} произведений исскуств</h2>
-            ) : (
-              <h2>Предлагаем к просмотру: {figures.length} произведений исскуств.</h2>
-            )}
-            <InfiniteScroll
-              style={{
-                height: 'auto',
-                overflow: 'auto',
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'center',
-              }}
-              dataLength={items.length}
-              next={fetchMoreData}
-              hasMore={hasMore}
-              loader={<h4>Loading...</h4>}
-              endMessage={<p>No more figures to display</p>}
-            >
-              {filteredFigures &&
-                filteredFigures.map((figure) => (
-                  <div key={figure.id}>
-                    <FigureItem figure={figure} key={figure.id} />
-                  </div>
-                ))}
-            </InfiniteScroll>
-          </div>
+        <div className='hpo'>
+        <div className='jhg'>
+          <CategoryPage />
+        </div>
+        <div className="results">
+          {isOpen ? (
+            <h2>По вашему запросу найдено: {filteredFigures.length} произведений исскуств</h2>
+          ) : (
+            <h2>Предлагаем к просмотру: {figures.length} произведений исскуств.</h2>
+          )}
+          <InfiniteScroll
+            style={{
+              height: 'auto',
+              overflow: 'auto',
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+            }}
+            dataLength={items.length}
+            next={fetchMoreData}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={<p>No more figures to display</p>}
+          >
+            {filteredFigures &&
+              filteredFigures.map((figure) => (
+                <div key={figure.id}>
+                  <FigureItem figure={figure} key={figure.id} />
+                </div>
+              ))}
+          </InfiniteScroll>
+        </div>
         </div>
       </div>
     </div>
