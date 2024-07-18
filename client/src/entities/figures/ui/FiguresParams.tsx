1c0b4901de0e098dcca65dc85f2359c25a589ca3
MashaShamash <mariapanasenko26@yandex.ru>
2024-07-18 11:09:19 +0300

    buttonLogout

--- client/src/entities/figures/ui/FiguresParams.tsx
+++ client/src/entities/figures/ui/FiguresParams.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useAppSelector } from '../../../app/store/store';
+import { useAppDispatch, useAppSelector } from '../../../app/store/store';
 import { Figure } from '../types/figureTypes';
 import './FiguresParams.css';
-
-
+import { addToBasket } from '../figuresSlice';
 
 function FiguresParams(): JSX.Element {
+  let user;
+  const dispatch = useAppDispatch();
   const { figId } = useParams<{ figId: string }>();
-
-
+  const [openBasket, setOpenBasket] = useState(false);
   const { figures } = useAppSelector((state) => state.figures);
   const figure = figures.find((f) => f.id === +figId);
-
 
   if (!figure) {
     return <p>Figure not found</p>;
   }
+  const handleAddToBasket = (): void => {
+    void dispatch(addToBasket(figure.id));
+    setOpenBasket(true);
+  };
 
   return (
-   
-      <div className="figureItem">
-        <img src={figure.img} alt='foto' />
-        <div className="figureItemText">
-        <p className='bold'>Название картины: {figure.title}</p>
+    <div className="figureItem">
+      <img src={figure.img} alt="foto" />
+      <div className="figureItemText">
+        <p className="bold">Название картины: {figure.title}</p>
         <p>Имя художника: {figure.name}</p>
         <p>Фамилия художника: {figure.lastName}</p>
         <p>Псевдоним художника: {figure.pseudonym}</p>
@@ -33,8 +35,22 @@ function FiguresParams(): JSX.Element {
         <p>Высота холста: {figure.height} см</p>
         <p>Ширина холста: {figure.width} см</p>
 
-        <p className='bold'>Стоимость: {figure.price} руб.</p>
-
+        <p className="bold">Стоимость: {figure.price} руб.</p>
+        <button
+          style={{
+            backgroundColor: 'transparent',
+            border: 'none',
+            padding: '10px',
+            border: 'solid 1px',
+            borderRadius: '10px',
+            fontSize: '18px',
+          }}
+          className="btn"
+          type="button"
+          onClick={() => handleAddToBasket()}
+        >
+          добавить в корзину
+        </button>
       </div>
     </div>
   );
